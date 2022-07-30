a66fd4f854b0e9d44f70cc3c521cf24a4e882780
Matt <mgstankov30@gmail.com>
2022-07-30 13:46:16 -0400

    creation of table, schema and seeds for leafsmart database

---
+++ leafsmart/db/schema/favourites.sql
@@ -0,0 +1,9 @@
+DROP TABLE IF EXISTS favourites CASCADE;
+CREATE TABLE favourites (
+  id BIGSERIAL  PRIMARY KEY NOT NULL,
+  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  geoname_id INTEGER NOT NULL,
+  display_name VARCHAR 255, DEFAULT TO city_name,
+  city_name VARCHAR(255) NOT NULL,
+  created_at TIMESTAMP WITHOUT TIME ZONE DEFAULT NOW()
+);
---
+++ leafsmart/db/seeds/favourites.sql
@@ -0,0 +1,5 @@
+INSERT INTO favourites (user_id, geoname_id, display_name, city_name) 
+  VALUES
+    (1, 6324729, 'Halifax', 'Halifax');
+    (1, 5946768, 'Edmonton', 'Edmonton');
+    (2, 6167865, 'Toronto', 'Toronto');
