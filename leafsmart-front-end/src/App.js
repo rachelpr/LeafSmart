634dfedd67dac8338fe17717008db6a8afb29789
Matt <mgstankov30@gmail.com>
2022-07-31 13:27:51 -0400

    restructure the app page to include a nav bar layout

--- leafsmart-front-end/src/App.js
+++ leafsmart-front-end/src/App.js
@@ -1,7 +1,9 @@
 import React, { Fragment, useState, useEffect } from "react";
+import Nav from "./components/Nav";
+import "./App.scss";
 
 const App = () => {
-  const [users, setUsers] = useState([]);
+  /* const [users, setUsers] = useState([]);
   const [favourites, setFavourites] = useState([]);
   useEffect(() => {
     const getAPI = () => {
@@ -28,71 +30,20 @@ const App = () => {
     };
     getAPI();
   }, []);
-
+ */
   return (
-    <Fragment>
-      <header>
-        <h1>Leaf Smart Biatch</h1>
-      </header>
-      <div className="form-container">
-        <h2>Add Favourite</h2>
-        <form method="POST" action="http://127.0.0.1:3001/favourites/new">
-        <div>
-            <label>Favourite ID</label>
-            <input type="text" name="favourite_id" required />
-          </div>
-          <div>
-            <label>User ID</label>
-            <input type="text" name="user_id" required />
-          </div>
-          <div>
-            <label>Geoname ID</label>
-            <input type="text" name="geoname_id" required />
-          </div>
-          <div>
-            <label>Display Name</label>
-            <input type="text" name="display_name" required />
-          </div>
-          <div>
-            <label>City Name</label>
-            <input type="text" name="city_name" required />
-          </div>
-          <div>
-            <button type="submit">Add Favourite</button>
-          </div>
-        </form>
-      </div>
-      <main>
-        <section>
-          {users.map((user) => {
-            return (
-              <>
-                <div className="user-container" key={user.user_id}>
-                  <h1>{user.first_name}</h1>
-                  <h1>{user.last_name}</h1>
-                  <h1>{user.email}</h1>
-                </div>
-              </>
-            );
-          })}
-          {favourites.map((favourite) => {
-            return (
-              <>
-                <div
-                  className="favourite-container"
-                  key={favourite.favourite_id}
-                >
-                  <h1>{favourite.user_id}</h1>
-                  <h1>{favourite.geoname_id}</h1>
-                  <h1>{favourite.city_name}</h1>
-                  <h1>{favourite.display_name}</h1>
-                </div>
-              </>
-            );
-          })}
-        </section>
-      </main>
-    </Fragment>
+   <main className="layout">
+    <section className="nav-bar">
+    <Nav />
+    </section>
+    <section className="widgets">
+    <div>
+      <h1>
+        Widgets
+      </h1>
+    </div>
+    </section>
+   </main>
   );
 };
 
