dd7166c12c5da5d49fb2186730ae3d35fc1d6a3a
Matt <mgstankov30@gmail.com>
2022-07-31 15:07:26 -0400

    small format change

--- leafsmart-front-end/src/App.js
+++ leafsmart-front-end/src/App.js
@@ -32,19 +32,17 @@ const App = () => {
   }, []);
  */
   return (
-   <main className="layout">
-    <section className="nav-bar">
-    <Nav />
-    </section>
-    <hr className="sidebar__separator sidebar--centered" />
-    <section className="widgets">
-    <div>
-      <h1>
-        Widgets
-      </h1>
-    </div>
-    </section>
-   </main>
+    <main className="layout">
+      <section className="nav-bar">
+        <Nav />
+      </section>
+      <hr className="sidebar__separator sidebar--centered" />
+      <section className="widgets">
+        <div>
+          <h1>Widgets</h1>
+        </div>
+      </section>
+    </main>
   );
 };
 
