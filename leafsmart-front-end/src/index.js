0727580ae7f37c1764985cc894fddf49b45b9a29
Matt <mgstankov30@gmail.com>
2022-07-31 15:09:54 -0400

    add browser routing and routes to the index page for the application

--- leafsmart-front-end/src/index.js
+++ leafsmart-front-end/src/index.js
@@ -2,10 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.scss';
 import App from './App';
+import About from './components/About'
+import Contact from "./components/Contact";
+import LogIn from "./components/LogIn";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="About" element={<About />} />
+      <Route path="Contact" element={<Contact />} />
+      <Route path="LogIn" element={<LogIn />} />
+    </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
