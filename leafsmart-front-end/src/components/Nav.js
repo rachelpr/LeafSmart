c5b84a6ace43e458209f2630c00b41695e107cd4
Matt <mgstankov30@gmail.com>
2022-07-31 13:47:23 -0400

    add in placeholders for log in, log out, favourites and about and contact links

--- leafsmart-front-end/src/components/Nav.js
+++ leafsmart-front-end/src/components/Nav.js
@@ -1,10 +1,20 @@
 import React from 'react'
+import "./Nav.scss"
 
 const Nav = () => {
   return (
-    <div>
-      <h1> Leaf Smart</h1>
-    </div>
+    <>
+      <section className='head-links'>
+      <h1> LeafSmart</h1>
+      <p>LogIn</p>
+      <p>LogOut</p>
+      <p>favourite</p>
+      </section>
+    <section className='foot-links'>
+      <p>about</p>
+      <p>contact</p>
+    </section>
+    </>
   )
 }
 
