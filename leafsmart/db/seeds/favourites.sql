3126bcfcc1e88d637cdf92df616a31b781a499bf
b1u3too <almadsen792@live.ca>
2022-08-01 16:11:08 +0000

    remove prior frontend copy of app

--- leafsmart/db/seeds/favourites.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO favourites (user_id, geoname_id, display_name, city_name) 
-  VALUES
-    (1, 6324729, 'Halifax', 'Halifax');
-    (1, 5946768, 'Edmonton', 'Edmonton');
-    (2, 6167865, 'Toronto', 'Toronto');
