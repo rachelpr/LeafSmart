b101aef5d4efd3b295cbd9c4f5055a9da2d049ed
Matt <mgstankov30@gmail.com>
2022-08-03 08:06:01 -0400

    add api call using axios as well as time specific params to pull in correct date and pass to eventList as a props for html formatting

--- leafsmart-front-end/src/components/Events.js
+++ leafsmart-front-end/src/components/Events.js
@@ -1,16 +1,23 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import EventListItem from './EventsList';
 
-// const today = new Date();
-// const date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+const today = new Date();
+const year = today.getFullYear()
+const day = ("0" + today.getDate()).slice(-2);
+const month = ("0" + (today.getMonth()+1)).slice(-2)
+const date = (year+"-"+month+"-"+day+"T19:00:00Z");
+
+console.log(date);
 
 const api = `https://app.ticketmaster.com/discovery/v2/events.json?apikey=6gMe63zUGZXgI1NElCvwwZZfzG8Soesu`;
 
 const Events = () => {
   const [events, setEvents] = useState([]);
 
+  //api call to pull in the events data from ticketmaster
   useEffect(() => {
-     axios.get(`${api}&city=toronto`)
+     axios.get(`${api}&city=toronto&sort=date,name,asc&startDateTime=${date}`)
     .then((res) => {
       const data = res.data['_embedded']['events'];
       setEvents(data);
@@ -20,19 +27,9 @@ const Events = () => {
     });
   }, []);
 
-  console.log(events[0]);
+  console.log(events)
 
-const EventListItem = (props) => {
-    return (
-      <li>
-        <img 
-        src={props.image}
-        alt={props.name}/>
-        <p>{props.selected}</p>
-      </li>
-    )
-  }
-
+  //mapping the data from the events array to showcase via the events list item breakdown.
   const eventsArr = events.map((events) => {
     return (
       <EventListItem
@@ -40,6 +37,7 @@ const EventListItem = (props) => {
         name={events.name}
         date={events.dates.start.localDate}
         image={events.images[0].url}
+        url={events.url}
         />
     );
   });
