c5f33baa643d79639d218cd012a9f13be4495c50
Matt <mgstankov30@gmail.com>
2022-08-04 15:38:10 -0400

    update login file to include a route, form, and handle submit for log in

--- leafsmart-front-end/src/components/LogIn.js
+++ leafsmart-front-end/src/components/LogIn.js
@@ -1,20 +1,69 @@
-import React from 'react'
-import Nav from "./Nav";
+import React, { useRef, useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
+import Cookies from 'universal-cookie';
 
-const LogIn = () => {
-  return (
-    <main className="layout">
-      <section className="nav-bar">
-        <Nav />
-      </section>
-      <hr className="sidebar__separator sidebar--centered" />
-      <section className="widgets">
-        <div>
-          <h1>Log In</h1>
-        </div>
-      </section>
-    </main>
-  )
+const cookies = new Cookies();
+
+export default function LogIn(props) {
+  const navigate = useNavigate();
+  const userRef = useRef();
+  const errRef = useRef();
+
+  const [email, setEmail] = useState("");
+  const [pwd, setPwd] = useState("");
+  const [errMsg, setErrMsg] = useState("");
+  const [userObj, setUserObj] = useState({});
+
+const handleSubmit = async (e) => {
+  e.preventDefault();
+
+  axios.post(`/login`, {
+    email: email,
+    password: pwd
+  })
+  .then((res) => {
+    const data = res.data[0];
+    cookies.set('user_id', res.data[0].user_id)
+    setUserObj(data)
+    navigate("/")
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 }
 
-export default LogIn
+  return (
+    <section>
+      <p
+        ref={errRef}
+        className={errMsg ? "errMsg" : "offscreen"}
+        aria-live="assertive"
+      >
+        {errMsg}
+      </p>
+      <h1>Please Log In</h1>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="email">Email:</label>
+        <input
+          type="text"
+          id="email"
+          ref={userRef}
+          autoComplete="off"
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+          required
+        />
+        <label htmlFor="password">Password:</label>
+        <input
+          type="password"
+          id="password"
+          onChange={(e) => setPwd(e.target.value)}
+          value={pwd}
+          required
+        />
+        <button type="submit">Submit</button>
+      </form>
+    </section>
+  );
+}
