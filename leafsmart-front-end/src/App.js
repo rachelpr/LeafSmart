93d912515caf4dbeb40e1e0a45ea9ced1cc7bbe4
Andrea Madsen <almadsen792@live.ca>
2022-08-03 20:25:34 -0600

    initialize Search component and integrate to App

--- leafsmart-front-end/src/App.js
+++ leafsmart-front-end/src/App.js
@@ -4,6 +4,7 @@ import Events from "./components/Events"
 import "./App.scss";
 import CityFacts from "./components/CityFacts";
 import QoLData from "./components/QoLData";
+import Search from "./components/Search";
 import './styles/main.css';
 
 const App = () => {
@@ -44,6 +45,7 @@ const App = () => {
       <section className="widgets">
         <div>
           <h1 className="text-3xl font-bold underline text-purple-600 hover:bg-blue-50">Widgets</h1>
+          <Search />
           <Events />
           <CityFacts />
           <QoLData />
