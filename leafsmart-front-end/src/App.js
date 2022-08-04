294dc2e3206420629269c3ae508eb4b99413aa73
Matt <mgstankov30@gmail.com>
2022-08-04 15:37:13 -0400

    remove unused code

--- leafsmart-front-end/src/App.js
+++ leafsmart-front-end/src/App.js
@@ -1,40 +1,10 @@
-// import React, { Fragment, useState, useEffect } from "react";
+import React, { Fragment, useState, useEffect } from "react";
 import Nav from "./components/Nav";
 import Events from "./components/Events"
 import "./App.scss";
-import CityFacts from "./components/CityFacts";
-import QoLData from "./components/QoLData";
-import './styles/main.css';
 
 const App = () => {
-  /* const [users, setUsers] = useState([]);
-  const [favourites, setFavourites] = useState([]);
-  useEffect(() => {
-    const getAPI = () => {
-      // Change this endpoint to whatever local or online address you have
-      // Local PostgreSQL Database
-      const userFetch = fetch("http://127.0.0.1:3001/");
-      const favouriteFetch = fetch("http://127.0.0.1:3001/favourites");
 
-      Promise.all([userFetch, favouriteFetch])
-        .then(async (response) => {
-          const userResponse = await response[0].json();
-          const favouriteResponse = await response[1].json();
-          console.log(response);
-          console.log(userResponse);
-          console.log(favouriteResponse);
-          setUsers(userResponse);
-          setFavourites(favouriteResponse);
-          return response.json();
-        })
-        // .then((data) => {
-        //   console.log(data);
-        // })
-        .catch((err) => console.log(err));
-    };
-    getAPI();
-  }, []);
- */
   return (
     <main className="layout">
       <section className="nav-bar">
@@ -43,10 +13,8 @@ const App = () => {
       <hr className="sidebar__separator sidebar--centered" />
       <section className="widgets">
         <div>
-          <h1 className="text-3xl font-bold underline text-purple-600 hover:bg-blue-50">Widgets</h1>
+          <h1>Widgets</h1>
           <Events />
-          <CityFacts />
-          <QoLData />
         </div>
       </section>
     </main>
