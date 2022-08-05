007d0e6ac59f9be0e3fd872f1730021e5fb02c7d
Matt <mgstankov30@gmail.com>
2022-08-05 17:11:22 -0400

    temp code to handle the log in log out functionality, intended as a short term fix

--- leafsmart-front-end/src/components/LogIn.js
+++ leafsmart-front-end/src/components/LogIn.js
@@ -1,69 +1,66 @@
-import React, { useRef, useEffect, useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import axios from "axios";
-import Cookies from 'universal-cookie';
+import Cookies from "universal-cookie";
+import Nav from "./Nav";
 
 const cookies = new Cookies();
 
 export default function LogIn(props) {
   const navigate = useNavigate();
-  const userRef = useRef();
-  const errRef = useRef();
 
   const [email, setEmail] = useState("");
   const [pwd, setPwd] = useState("");
-  const [errMsg, setErrMsg] = useState("");
-  const [userObj, setUserObj] = useState({});
+  const [userObj, setUserObj] = useState(null || cookies.get("user"));
 
-const handleSubmit = async (e) => {
-  e.preventDefault();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-  axios.post(`/login`, {
-    email: email,
-    password: pwd
-  })
-  .then((res) => {
-    const data = res.data[0];
-    cookies.set('user_id', res.data[0].user_id)
-    setUserObj(data)
-    navigate("/")
-  })
-  .catch((err) => {
-    console.log(err);
-  });
-}
+    axios
+      .post(`/login`, {
+        email: email,
+        password: pwd,
+      })
+      .then((res) => {
+        const data = res.data[0];
+        cookies.set("user", data, {maxAge: 600});
+        setUserObj(data);
+        navigate("/");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
   return (
-    <section>
-      <p
-        ref={errRef}
-        className={errMsg ? "errMsg" : "offscreen"}
-        aria-live="assertive"
-      >
-        {errMsg}
-      </p>
-      <h1>Please Log In</h1>
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="email">Email:</label>
-        <input
-          type="text"
-          id="email"
-          ref={userRef}
-          autoComplete="off"
-          onChange={(e) => setEmail(e.target.value)}
-          value={email}
-          required
-        />
-        <label htmlFor="password">Password:</label>
-        <input
-          type="password"
-          id="password"
-          onChange={(e) => setPwd(e.target.value)}
-          value={pwd}
-          required
-        />
-        <button type="submit">Submit</button>
-      </form>
-    </section>
+    <main className="layout">
+      <section className="nav-bar">
+        <Nav user={userObj} />
+      </section>
+      <hr className="sidebar__separator sidebar--centered" />
+      <section className="widgets">
+        <h1>Please Log In</h1>
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="email">Email:</label>
+          <input
+            type="text"
+            id="email"
+            autoComplete="off"
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
+            required
+          />
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            onChange={(e) => setPwd(e.target.value)}
+            value={pwd}
+            required
+          />
+          <button type="submit">Submit</button>
+        </form>
+      </section>
+    </main>
   );
 }
--- leafsmart-front-end/src/components/Nav.js
+++ leafsmart-front-end/src/components/Nav.js
@@ -1,71 +1,48 @@
 import React, { useEffect, useState } from "react";
-import axios from "axios";
 import { Link } from "react-router-dom";
 import "./Nav.scss";
 import Cookies from "universal-cookie";
 
-const cookie = new Cookies();
+const cookies = new Cookies();
 
-const Nav = () => {
-  const [user, setUser] = useState([]);
-  const [cookies, setCookies] = useState("");
-  const [account, setAccount] = useState("");
-  const [logged, setLogged] = useState("")
+const Nav = (props) => {
+  const [cookie, setCookie] = useState('')
+  const { user } = props;
+
+  const deleteCookie = (name) => cookie.remove(name);
 
   useEffect(() => {
-    axios
-      .get(`http://localhost:3001/`)
-      .then((res) => {
-        const data = res.data;
-        setUser(data);
-        cookie.set('user', data[0].user_id)
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+    setCookie(cookies.get("user"))
   }, []);
-
-  useEffect(() => {
-    setCookies(cookie.get("user_id"));
-    setAccount(cookie.get("user"));
-  }, []);
-
-  useEffect(() => {
-    setLogged(user[0]);
-  }, [user]);
 
   return (
     <>
-      {cookies === account? (
-        <>
-          <section className="head-links">
-            <Link to="/">
-              <h1>LeafSmart</h1>
-            </Link>
-            <Link to="/Favourites">Tina Testerson</Link>
-            <button type="submit" onClick={() => {setAccount("")}}>Log Out</button>
-            <p>Favourite</p>
-          </section>
-          <section className="foot-links">
-            <Link to="/About">About</Link>
-            <Link to="/Contact">Contact</Link>
-          </section>
-        </>
-      ) : (
-        <>
-          <section className="head-links">
-            <Link to="/">
-              <h1>LeafSmart</h1>
-            </Link>
-            <Link to="/LogIn">Log-In</Link>
-            <p>favourite</p>
-          </section>
-          <section className="foot-links">
-            <Link to="/About">About</Link>
-            <Link to="/Contact">Contact</Link>
-          </section>
-        </>
-      )}
+      <section className="head-links">
+        <Link className="block" to="/">
+          <h1>LeafSmart</h1>
+        </Link>
+        { cookie ? (
+          <>
+            <p>Tina Testerson</p>
+            <button
+              className="block"
+              type="submit"
+              onClick={() => {
+                setCookie('');
+              }}
+            >
+              Log Out
+            </button>
+          </>
+        ) : (
+          <Link to="/LogIn">Log-In</Link>
+        )}
+        <p>Favourite</p>
+      </section>
+      <section className="foot-links">
+        <Link to="/About">About</Link>
+        <Link to="/Contact">Contact</Link>
+      </section>
     </>
   );
 };
