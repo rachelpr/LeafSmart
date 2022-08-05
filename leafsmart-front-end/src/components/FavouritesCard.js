b0a043b0eec7495244bc89aeae503aa694423e65
Matt <mgstankov30@gmail.com>
2022-08-05 13:12:06 -0400

    add a card to funnel and display favourites as they are returned from the database

--- leafsmart-front-end/src/components/FavouritesCard.js
+++ leafsmart-front-end/src/components/FavouritesCard.js
@@ -2,8 +2,10 @@ import React from "react";
 
 export default function FavouritesCard(props) {
   return (
-    <li>
-      <h1>{props.name}</h1>
-    </li>
+    <div>
+      <ul className="block px-4 py-2 text-gray-800 hover:bg-indigo-500 hover:text-white">
+      {props.name}
+        </ul>
+    </div>
   );
 }
