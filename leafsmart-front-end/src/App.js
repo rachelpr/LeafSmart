f7c282044b9f09113bfb7a607334ad8656637faa
Andrea Madsen <almadsen792@live.ca>
2022-08-05 21:42:44 -0600

    Implement state for city search selected

--- leafsmart-front-end/src/App.js
+++ leafsmart-front-end/src/App.js
@@ -1,18 +1,28 @@
+import { useState } from "react";
+
 import Nav from "./components/Nav";
 import Events from "./components/Events"
 import CityFacts from "./components/CityFacts";
 import QoLData from "./components/QoLData";
 import Search from "./components/Search";
 
-import './styles/main.css';
+import "./styles/main.css";
 import "./App.scss";
 
 
 const App = () => {
+  const [cityName, setCityName] = useState(null);
+  const [coordinates, setCoordinates] = useState({});
+  const [geonameId, setGeonameId] = useState(null);
 
   const handleOnSearchChange = (searchData) => {
-    //response contains data such as {value: '5946768 53.55014 -113.46871', label: 'Edmonton, Alberta'}
-      console.log(searchData);
+    //sample searchData: {value: '5946768 53.55014 -113.46871', label: 'Edmonton, Alberta'}
+    const lowercasedCity = searchData.label.split(",")[0].toLowerCase();
+    const [geo, lat, lon] = searchData.value.split(" ");
+
+    setCityName(lowercasedCity);
+    setCoordinates({lat: lat, lon: lon});
+    setGeonameId(geo);
   }
 
   return (
@@ -25,7 +35,9 @@ const App = () => {
         <div>
           <h1 className="text-3xl font-bold underline text-purple-600 hover:bg-amber-50">Widgets</h1>
           <Search onSearchChange={handleOnSearchChange}/>
-          <CityFacts />
+          <CityFacts
+            geonameId={geonameId}
+          />
           <Events />
           <QoLData />
         </div>
