bd473e0d697ca78bc703d3ac14e31ab7f4ff1def
Andrea Madsen <almadsen792@live.ca>
2022-08-07 09:57:18 -0600

    add color palette Rachel provided

--- leafsmart-front-end/tailwind.config.js
+++ leafsmart-front-end/tailwind.config.js
@@ -4,6 +4,12 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
+    colors: {
+      'Isabelline':'#F2E9E4',
+      'Independence': '#4A4E69',
+      'Heliotrope Gray': '#9A8C98',
+      'Space Cadet': '#22223B'
+    },
     extend: {},
   },
   plugins: [],
