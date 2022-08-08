fe01223e2f0c6a80e0f64c5f0b7ee3b0023a5617
Matt <mgstankov30@gmail.com>
2022-08-07 20:02:47 -0400

    add current state of about page styling, needs more work

--- leafsmart-front-end/src/components/About.js
+++ leafsmart-front-end/src/components/About.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react";
 import Nav from "./Nav";
 
 const About = () => {
@@ -7,14 +7,43 @@ const About = () => {
       <section className="nav-bar">
         <Nav />
       </section>
-      <hr className="sidebar__separator sidebar--centered" />
       <section className="widgets">
-        <div>
-          <h1>About</h1>
+        <div class="px-16 mt-16 bg-Isabelline h-full">
+          <h1 className="text-9xl">About LeafSmart</h1>
+          <h2 className="text-5xl">
+            The Perfect place to get your travel location details before you
+            "leaf"!
+          </h2>
+          <div class="bg-[#7c9bdb] flex rounded-full p-4">
+            <div class="flex items-center text-white font-black text-base">
+              LeafSmart is the hot new app to get all the details about the
+              locations you want to visit.
+              <br />
+              4+ location based api's at your fingertips
+
+            </div>
+          </div>
+          <div class="bg-[#7c9bdb] flex rounded-full p-4">
+            <div class="flex items-center text-white font-black text-base">
+              <div>
+                <ul>
+                  <li>Events</li>
+                  <li>Weather</li>
+                  <li>Quality of life</li>
+                  <li>Handy Dandy Search bar!</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div class="bg-[#7c9bdb] flex rounded-full p-4">
+            <div class="flex items-center text-white font-black text-base">
+              Version - 1.0.0
+            </div>
+          </div>
         </div>
       </section>
     </main>
-  )
-}
+  );
+};
 
-export default About
+export default About;
