202a7abec6c73fd2ddf998544afbae779a42f042
Andrea Madsen <almadsen792@live.ca>
2022-08-07 22:58:30 -0600

    Clean up no-longer available classes and markup

--- leafsmart-front-end/src/components/Contact.js
+++ leafsmart-front-end/src/components/Contact.js
@@ -3,18 +3,15 @@ import Nav from "./Nav";
 
 const Contact = () => {
   return (
-    <main className="layout">
-      <section className="nav-bar">
-        <Nav />
-      </section>
-      <hr className="sidebar__separator sidebar--centered" />
-      <section>
+    <main className="page-container">
+      <Nav />
+      <section className="bg-Isabelline">
         <div>
           <h1>Contact</h1>
         </div>
       </section>
     </main>
-  )
+  );
 }
 
 export default Contact
