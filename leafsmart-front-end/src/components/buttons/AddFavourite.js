bd561cbe44200a51c13cd543bf5e085980b88f35
Andrea Madsen <almadsen792@live.ca>
2022-08-08 22:25:17 -0600

    animate button on hover

--- leafsmart-front-end/src/components/buttons/AddFavourite.js
+++ leafsmart-front-end/src/components/buttons/AddFavourite.js
@@ -1,7 +1,8 @@
 const AddFavourite = (props) => {
   const { icon } = props;
   return (
-    <div className="w-16 h-16 bg-gradient-to-r from-Independence to-HeliotropeGray text-White flex items-center justify-center rounded-2xl hover:text-Isabelline hover:rounded-lg">{icon}</div>
+    <div className="w-16 h-16 bg-gradient-to-r from-Independence to-HeliotropeGray text-White flex items-center justify-center rounded-2xl hover:text-Isabelline hover:rounded-lg transition-all duration-300 ease-linear
+    cursor-pointer">{icon}</div>
   );
 }
 
