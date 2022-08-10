b17911e366f225e16a32145777e862496a49c6a6
Matt <mgstankov30@gmail.com>
2022-08-10 17:03:28 -0400

    update button component to take in props and pass the information on click to Auth Context to be sent as a post request to the back end

--- leafsmart-front-end/src/components/buttons/AddFavourite.js
+++ leafsmart-front-end/src/components/buttons/AddFavourite.js
@@ -1,8 +1,28 @@
+import { useAuth } from "../../contexts/AuthContext";
+import { useEffect, useState } from "react";
+
 const AddFavourite = (props) => {
+  const [geoID, setGeoID] = useState(null)
+  const [displayName, setDisplayName] = useState("")
+  const [cityName, setCityName] = useState("")
+
+  const { saveFavourites } = useAuth();
+
+  useEffect(() => {
+    setGeoID(props.geonameId);
+    setDisplayName(props.cName);
+    setCityName(props.cName);
+  }, [props]);
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    saveFavourites(geoID, displayName, cityName)
+  }
+
   const { icon } = props;
   return (
     <div className="w-16 h-16 bg-gradient-to-r from-Independence to-HeliotropeGray text-White flex items-center justify-center rounded-2xl hover:text-Isabelline hover:rounded-lg transition-all duration-300 ease-linear
-    cursor-pointer">{icon}</div>
+    cursor-pointer"><button onClick={handleClick}>{icon}</button></div>
   );
 }
 
