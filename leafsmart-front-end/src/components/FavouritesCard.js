755e1f2fd66b3d34cfa1d42d0e3406f4f9a93e7e
Matt <mgstankov30@gmail.com>
2022-08-10 17:01:32 -0400

    add a small function to capatilize the first letter of the returned favourites so they are all properly displayed

--- leafsmart-front-end/src/components/FavouritesCard.js
+++ leafsmart-front-end/src/components/FavouritesCard.js
@@ -1,10 +1,17 @@
 import React from "react";
 
+function capitalizeFirstLetter(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
 export default function FavouritesCard(props) {
+
+  const name = capitalizeFirstLetter(props.name);
+  
   return (
     <div>
       <ul className="block px-4 py-2 hover:bg-Independence hover:text-White">
-        {props.name}
+        {name}
       </ul>
     </div>
   );
