dc44d68210bd0e4d75a966e26ee7f8021d9577e3
Andrea Madsen <almadsen792@live.ca>
2022-08-13 22:42:37 -0600

    Only render image if data present

--- leafsmart-front-end/src/components/cityData/CityFacts.js
+++ leafsmart-front-end/src/components/cityData/CityFacts.js
@@ -3,15 +3,18 @@ import NumberFormat from "react-number-format";
 const CityFacts = (props) => {
   const { cityName, cityPop, imageData } = props;
 
+  console.log("imageData Keys: ", Object.keys(imageData).length);
   return (
     <section>
       { cityName && (
         <section>
-          <img
-            className="rounded-2xl mb-6"
-            src={imageData.web}
-            alt={`Photographer ${imageData.attribution.photographer} on ${imageData.attribution.site}, licensed under ${imageData.attribution.license}`}
-          />
+          { Object.keys(imageData).length > 0 && (
+            <img
+              className="rounded-2xl mb-6"
+              src={imageData.web}
+              alt={`Photographer ${imageData.attribution.photographer} on ${imageData.attribution.site}, licensed under ${imageData.attribution.license}`}
+            />
+          )}
           <h2 className="text-2xl font-bold">{cityName}</h2>
           <p className="inline">Population: </p>
           <NumberFormat
