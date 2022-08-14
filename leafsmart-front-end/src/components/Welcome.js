1fcd3d7b3d6bdb943d78f833f6e2ca115d0be19c
Andrea Madsen <almadsen792@live.ca>
2022-08-13 18:50:42 -0600

    prelim typewriter setup

--- leafsmart-front-end/src/components/Welcome.js
+++ leafsmart-front-end/src/components/Welcome.js
@@ -1,10 +1,28 @@
+import Typewriter from 'react-ts-typewriter';
+
 const Welcome = () => {
+  const copyLoop = [
+    "concerts",
+    "games",
+    "expos",
+    "weather",
+    "stats"
+  ];
+
   return (
-    <section className="mt-6 bg-White w-100 rounded-3xl p-16">
-        <p>Ready to <i>leaf</i>?</p>
-        <p>You won't be<i>leaf</i> what you'll find</p>
-        <p><i>Leaf</i> behind your worries</p>
-        <p><i>Leaf</i> the searching to us</p>
+    <section className="flex justify-evenly items-center mt-6 bg-White w-100 rounded-3xl p-16">
+      <img
+        className="h-48 bg "        src="LeafSmartLight.png"
+        alt="Leafsmart Logo"
+      />
+      <div>
+        <p>Check the</p>
+        <Typewriter
+          text={copyLoop}
+          loop="true"
+        />
+        <p>before you leaf!</p>
+      </div>
     </section>
   );
 }
