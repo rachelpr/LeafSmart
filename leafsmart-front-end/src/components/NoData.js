0d58cd8f792510ec4ea9bfc36d9bf5474abcacc9
Andrea Madsen <almadsen792@live.ca>
2022-08-13 21:29:42 -0600

    load random 1 of 5 svgs if no data found

--- leafsmart-front-end/src/components/NoData.js
+++ leafsmart-front-end/src/components/NoData.js
@@ -1,7 +1,15 @@
 const NoData = (props) => {
   const { widgetName } = props;
+  const num = Math.floor(Math.random() * 5);
 
-  return (`No data for ${widgetName}`);
+  return (
+    <section>
+      <h3>
+      No data for {widgetName}
+      </h3>
+      <img src={`images/undraw_${num}.svg`} alt="No results found!"/>
+    </section>
+    );
 }
 
 export default NoData;
