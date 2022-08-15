f11fc38317739745843f5aa7698749b0ec0bbccd
Matt <mgstankov30@gmail.com>
2022-08-15 18:17:23 -0400

    remove hover styling and cursor styling for the home button to make it static

--- leafsmart-front-end/src/components/buttons/HomeIcon.js
+++ leafsmart-front-end/src/components/buttons/HomeIcon.js
@@ -5,9 +5,7 @@ const HomeIcon = (props) => {
     h-20 w-20 m-auto shadow-lg
     hover:bg-Independence hover:text-White
     bg-gradient-to-r from-Independence to-HeliotropeGray
-    rounded-3xl hover:rounded-xl
-    transition-all duration-300 ease-linear
-    cursor-pointer">{icon}</div>
+    rounded-xl">{icon}</div>
   );
 }
 
