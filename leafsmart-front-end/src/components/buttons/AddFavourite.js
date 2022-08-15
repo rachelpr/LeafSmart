e5958804d811c2bb0c01940d4f956da0e43bf566
Matt <mgstankov30@gmail.com>
2022-08-15 09:38:06 -0400

    add functionality to favourite button to disable if a favourite is in the users list of favourites, moved returnFavourites to run outside of the click on the faovurite drop down to access easier

--- leafsmart-front-end/src/components/buttons/AddFavourite.js
+++ leafsmart-front-end/src/components/buttons/AddFavourite.js
@@ -2,18 +2,30 @@ import { useAuth } from "../../contexts/AuthContext";
 import { useEffect, useState } from "react";
 
 const AddFavourite = (props) => {
-  const { geonameId, cName } = props
-  const [geoID, setGeoID] = useState(null)
-  const [displayName, setDisplayName] = useState("")
-  const [cityName, setCityName] = useState("")
+  const { geonameId, cName } = props;
+  const [geoID, setGeoID] = useState(null);
+  const [displayName, setDisplayName] = useState("");
+  const [cityName, setCityName] = useState("");
 
-  const { saveFavourites } = useAuth();
+  const { saveFavourites, favourites, returnFavourites } = useAuth();
 
   useEffect(() => {
     setGeoID(geonameId);
     setDisplayName(cName);
     setCityName(cName);
+    returnFavourites();
   }, [geonameId, cName]);
+  
+  const favouritesFilter = (fav) => {
+    const cityNameArr = [];
+    fav.forEach((item) => {
+      cityNameArr.push(item.city_name);
+    });
+    return cityNameArr;
+  };
+
+  const cityArr = favouritesFilter(favourites);
+
 
   const handleClick = (e) => {
     e.preventDefault();
@@ -22,13 +34,24 @@ const AddFavourite = (props) => {
 
   const { icon } = props;
   return (
-      <button
-        className="w-16 h-16 bg-gradient-to-r from-Independence to-HeliotropeGray text-White flex items-center justify-center rounded-2xl hover:text-Isabelline hover:rounded-lg transition-all duration-300 ease-linear
-        cursor-pointer active:bg-red-500"
-        onClick={handleClick}
-      >
-        {icon}
-      </button>
+<>
+      {cityArr.includes(cityName) ? (
+        <button
+          className="w-16 h-16 bg-gradient-to-r from-Independence to-HeliotropeGray text-White flex items-center justify-center rounded-2xl opacity-50"
+          type="button"
+          disabled={true}
+        >
+          {icon}
+        </button>
+      ) : (
+        <div
+          className="w-16 h-16 bg-gradient-to-r from-Independence to-HeliotropeGray text-White flex items-center justify-center rounded-2xl hover:text-Isabelline hover:rounded-lg transition-all duration-300 ease-linear
+  cursor-pointer"
+        >
+          <button onClick={handleClick}>{icon}</button>
+        </div>
+      )}
+    </>
   );
 }
 
